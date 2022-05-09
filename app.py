99c327bc47c0446bca560d0e2fe8365f7b5279f7
hoinlee-moi <lhi3048@gmail.com>
2022-05-09 16:43:11 +0900

    feat : 배너 이미지 맞춤 posting 페이지 연결 지도와 이미지 업데이트 진행중

--- app.py
+++ app.py
@@ -170,5 +170,16 @@ def get_posts():
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
+@app.route('/posting/<username>')
+def posting(username):
+    return render_template("posting.html" )
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
