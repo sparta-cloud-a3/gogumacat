e6e7eb07bd943b0c10eff3df8db50e21fdd72d67
hongggs <hongseoda@gmail.com>
2022-05-11 14:06:14 +0900

    modify: 주소별 검색 법정동으로 변경

--- app.py
+++ app.py
@@ -503,7 +503,16 @@ def get_dong():
         dong = list(db.korea_address.distinct('dong', {'gu': gu}))
     return jsonify({'dong': dong})
 
+@app.route('/search/address', methods=['GET'])
+def search_by_address():
+    si = request.args.get('si')
+    gu = request.args.get('gu')
+    dong = request.args.get('dong')
+    print(si, gu, dong)
 
+
+
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
